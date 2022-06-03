4d6ded119badce0f2a24ec16e7afa633212d61ce
Rodrigo Palza Gonzalez <programaro07@gmail.com>
2022-06-02 21:49:34 -0400

    se agregan los bloques try/catch para manejo de errores con respuestas de estado http

--- servidor.js
+++ servidor.js
@@ -9,68 +9,110 @@ http.createServer( async(req, res) => {
 
     // Ruta para leer archivo HTML
     if(req.url ==='/' && req.method === 'GET') {
-        res.setHeader('Content-Type', 'text/html')
-        res.end(fs.readFileSync('index.html', 'utf-8'))
+        try {
+            res.setHeader('Content-Type', 'text/html')
+            res.statusCode = 200
+            res.end(fs.readFileSync('index.html', 'utf-8'))
+        } catch (error) {
+            res.statusCode = 500
+            res.end(error.message)
+        }
     }
 
     // Ruta para obtener registros de transferencias
     if(req.url === '/transferencias' && req.method === 'GET') {
-        const registros = await consultarTransferencias()
-        res.end(JSON.stringify(registros))
+        try {
+            const registros = await consultarTransferencias()
+            res.statusCode = 200
+            res.end(JSON.stringify(registros))
+        } catch (error) {
+            res.statusCode = 500
+            res.end(error.message)
+        }
     }
 
     // Ruta para realizar transferencias
     if(req.url === '/transferencia' && req.method === 'POST') {
-        let body = ''
-        req.on('data', (chunk) => {
-            body += chunk
-        })
-        req.on('end', async () => {
-            const datos = JSON.parse(body)
-            await hacerTransferencias(datos)
-            res.end(JSON.stringify({}))
-        })
+        try {
+            let body = ''
+            req.on('data', (chunk) => {
+                body += chunk
+            })
+            req.on('end', async () => {
+                const datos = JSON.parse(body)
+                await hacerTransferencias(datos)
+                res.statusCode = 201
+                res.end(JSON.stringify({}))
+            })
+        } catch (error) {
+            res.statusCode = 500
+            res.end(error.message)
+        }
     }
 
     // Ruta para crear nuevos usuarios
     if(req.url === '/usuario' && req.method === 'POST') {
-        let body = ''
-        req.on('data', (chunk) => {
-            body += chunk
-        })
-        req.on('end', async () => {
-            const datos = Object.values(JSON.parse(body))
-            const respuesta = await crearUsuarios(datos)
-            res.end(JSON.stringify(respuesta))
-        })
+        try {
+            let body = ''
+            req.on('data', (chunk) => {
+                body += chunk
+            })
+            req.on('end', async () => {
+                const datos = Object.values(JSON.parse(body))
+                const respuesta = await crearUsuarios(datos)
+                res.statusCode = 201
+                res.end(JSON.stringify(respuesta))
+            })
+        } catch (error) {
+            res.statusCode = 500
+            res.end(error.message)
+        }
     }
 
     // Ruta para obtener registros de usuarios
     if(req.url === '/usuarios' && req.method === 'GET') {
-        const registros = await consultarUsuarios()
-        res.end(JSON.stringify(registros))
+        try {
+            const registros = await consultarUsuarios()
+            res.statusCode = 200
+            res.end(JSON.stringify(registros))
+        } catch (error) {
+            res.statusCode = 500
+            res.end(error.message)
+        }
     }
 
     // Ruta para actualizar registros de usuarios
     if(req.url.startsWith('/usuario') && req.method === 'PUT') {
-        const { id } = url.parse(req.url, true).query
-        let body = ''
-        req.on('data', (chunk) => {
-            body += chunk
-        })
-        req.on('end', async () => {
-            const datos = JSON.parse(body)
-            console.log(datos)
-            const respuesta = await actualizarUsuarios(datos, id)
-            res.end(JSON.stringify(respuesta))
-        })
+        try {
+            const { id } = url.parse(req.url, true).query
+            let body = ''
+            req.on('data', (chunk) => {
+                body += chunk
+            })
+            req.on('end', async () => {
+                const datos = JSON.parse(body)
+                console.log(datos)
+                const respuesta = await actualizarUsuarios(datos, id)
+                res.statusCode = 200
+                res.end(JSON.stringify(respuesta))
+            })
+        } catch (error) {
+            res.statusCode = 500
+            res.end(error.message)
+        }
     }
 
     // Ruta para eliminar un usuario
     if(req.url.startsWith('/usuario') && req.method === 'DELETE') {
-        const { id } = url.parse(req.url, true).query
-        const respuesta = await eliminarUsuarios(id)
-        res.end(JSON.stringify(respuesta))
+        try {
+            const { id } = url.parse(req.url, true).query
+            const respuesta = await eliminarUsuarios(id)
+            res.statusCode = 200
+            res.end(JSON.stringify(respuesta))
+        } catch (error) {
+            res.statusCode = 500
+            res.end(error.message)
+        }
     }
 
 }).listen(3000, console.log('Server ON!'))
